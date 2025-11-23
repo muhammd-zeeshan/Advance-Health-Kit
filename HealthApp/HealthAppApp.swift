900ade59150a9e25e6c2626b54ccbea0076ce29f
Muhammad Zeeshan <rafaq657@gmail.com>
2025-11-23 21:26:06 +0500

    Initial Commit

---
+++ HealthApp/HealthAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAppApp.swift
+//  HealthApp
+//
+//  Created by IOS-Developer on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
